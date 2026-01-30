ee7eb2c48e45e3a34e14afc4d1b02a99d8b52c0a
deakcs <deakcs@310-T>
2026-01-30 09:19:54 +0100

    Add project files.

---
+++ Raktarkez/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Raktarkez
+{
+    public class Termek
+    {
+        protected string nev;
+        private int ar;
+
+        public Termek(string nev, int ar)
+        {
+            this.nev = nev;
+            this.ar = ar;
+        }
+        public void Info()
+        {
+            Console.WriteLine($"A termék neve: {nev}; ára: {ar} Ft");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Termek a = new Termek("sajt", 1000);
+            a.Info();
+        }
+    }
+}
