90d2204643499075048130d72a2f8ab68f158f5a
deakcs <deakcs@310-T>
2026-01-30 09:32:25 +0100

    Gyenes Krisztián

--- Raktarkez/Program.cs
+++ Raktarkez/Program.cs
@@ -23,13 +23,23 @@
         {
             this.lejaratiSzam = lejaratiSzam;
         }
+        public void LejartE()
+        {
+            if (lejaratiSzam < 10)
+                Console.WriteLine("Lejárt a termék");
+        }
     }
+
+  
     internal class Program
     {
         static void Main(string[] args)
         {
             Termek a = new Termek("sajt", 1000);
             a.Info();
+            Elelmiszer e = new Elelmiszer("tej", 300, 8);
+            e.Info();
+            e.LejartE();
         }
     }
 }
